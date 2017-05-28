6a2d460b65249052eea709b612d225820c6ba827
SeanB1995 <x15561177@student.ncirl.ie>
2017-05-28 23:35:30 +0100

    Declared/Created class

--- app/src/main/java/sb/fingalge16/MainActivity.java
+++ app/src/main/java/sb/fingalge16/MainActivity.java
@@ -3,11 +3,13 @@ package sb.fingalge16;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.PieChart;
+
 public class MainActivity extends AppCompatActivity {
 
     private float[] yData = {25.3f, 12.3f};
     private String[] xData = {"Barry Martin", "a"};
-
+    PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
